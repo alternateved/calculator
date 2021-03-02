7863782b9b0b81f43ec5d93d7126f4871229bb98
alternateved <alternateved@gmail.com>
2021-03-02 17:40:47 +0100

    Add basic math functions

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (a === 0 && b === 0) {
+    return "NOPE";
+  } else return a / b;
+}

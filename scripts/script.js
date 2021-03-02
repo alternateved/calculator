8ba23f7153596ee726329cb86ca4317caa130614
alternateved <alternateved@gmail.com>
2021-03-02 18:13:06 +0100

    Add function operate

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,22 @@ function divide(a, b) {
     return "NOPE";
   } else return a / b;
 }
+
+function operate(a, b, operator) {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+  return result;
+}
